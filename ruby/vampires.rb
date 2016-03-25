f3d4aec14d7f255cca26bef526626b5ee837c971
whatthemandy <amcdorman@gmail.com>
2016-03-25 11:50:52 -0500

    add loop around everything

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,66 +1,76 @@
-puts "What is your name?"
-name = gets.chomp
+puts "How many employees will be processed?"
+number_of_employees = gets.chomp
+number_of_employees = number_of_employees.to_i
 
-puts "How old are you?"
-num = gets.chomp
-num = num.to_i
+until number_of_employees == 0
 
-puts "What year were you born?"
-year = gets.chomp
-year = year.to_i
+  puts "What is your name?"
+  name = gets.chomp
 
-  if (2016-year)==num
-    correct_age=true
+  puts "How old are you?"
+  num = gets.chomp
+  num = num.to_i
+
+  puts "What year were you born?"
+  year = gets.chomp
+  year = year.to_i
+
+    if (2016-year)==num
+      correct_age=true
+    else
+      correct_age=false #&& incorrect_age=true
+    end
+
+    if (2016-year)==num
+      incorrect_age=false
+    else
+      incorrect_age=true
+    end
+
+  puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+  input = gets.chomp
+
+    if input=="yes" || input=="y"
+        likes_garlic=true
+    else
+        likes_garlic=false #&& hates_garlic=true
+    end
+
+    if input=="yes" || input=="y"
+        hates_garlic=false
+    else
+        hates_garlic=true
+    end
+
+  puts "Would you like to enroll in the company’s health insurance?"
+  input = gets.chomp
+
+    if input=="yes" || input=="y"
+        wants_insurance=true
+    else
+        wants_insurance=false #&& waives_insurance==true
+    end
+
+    if input=="yes" || input=="y"
+        waives_insurance=false
+    else
+        waives_insurance=true
+    end
+
+  if name=="Drake Cula"
+    puts "Definitely a vampire."
+  elsif name=="Tu Fang"
+    puts "Definitely a vampire."
+  elsif correct_age && (likes_garlic || wants_insurance)
+    puts "Probably not a vampire."
+  elsif incorrect_age && (hates_garlic && waives_insurance)
+    puts "Almost certainly a vampire."
+   elsif incorrect_age && (hates_garlic || waives_insurance)
+    puts "Probably a vampire."
   else
-    correct_age=false #&& incorrect_age=true
+    puts "Results inconclusive."
   end
 
-  if (2016-year)==num
-    incorrect_age=false
-  else
-    incorrect_age=true
-  end
+  number_of_employees -= 1
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
-input = gets.chomp
-
-  if input=="yes" || input=="y"
-      likes_garlic=true
-  else
-      likes_garlic=false #&& hates_garlic=true
-  end
-
-  if input=="yes" || input=="y"
-      hatess_garlic=false
-  else
-      hates_garlic=true
-  end
-
-puts "Would you like to enroll in the company’s health insurance?"
-input = gets.chomp
-
-  if input=="yes" || input=="y"
-      wants_insurance=true
-  else
-      wants_insurance=false #&& waives_insurance==true
-  end
-
-  if input=="yes" || input=="y"
-      waives_insurance=false
-  else
-      waives_insurance=true
-  end
-
-if name=="Drake Cula"
-  puts "Definitely a vampire."
-elsif name=="Tu Fang"
-  puts "Definitely a vampire."
-elsif correct_age && (likes_garlic || wants_insurance)
-  puts "Probably not a vampire."
-elsif incorrect_age && (hates_garlic && waives_insurance)
-  puts "Almost certainly a vampire."
- elsif incorrect_age && (hates_garlic || waives_insurance)
-  puts "Probably a vampire."
-else
-  puts "Results inconclusive."
 end
