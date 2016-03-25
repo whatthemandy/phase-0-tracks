12a466905b805c8e168767d4ab13a0edbf7ba0ee
whatthemandy <amcdorman@gmail.com>
2016-03-24 22:45:48 -0500

    ask questions and store in variables

---
+++ ruby/vampires.rb
@@ -0,0 +1,18 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp
+
+puts "What year were you born?"
+year = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+insurance = gets.chomp
+
+
+
+
