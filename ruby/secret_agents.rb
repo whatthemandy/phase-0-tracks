287d406695750817cc010b43e98f15600ec90c3a
whatthemandy <amcdorman@gmail.com>
2016-03-27 11:50:58 -0500

    update file, merge conflict?

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,57 +1,69 @@
-# - get a string
-# - Write a method with a loop inside of it
-# - Loop through each letter of the string, using .next for each one
-# - At the end, print the final string#
-
+# Write a method with a loop inside of it
+# Loop through each letter of the string, use .next to change each one to the following letter
+# Add one to the index to continue to the next letter until you hit the last letter of the string
+# Print the final string
 
 def encrypt(string)
   index = 0
-    while index < string.length
-      string[index] = string[index].next
-      index += 1
+  while index < string.length
+    if string[index] == "z"
+      string[index] = "a"
+    elsif string[index] == " "
+      string[index] = " "
+    else string[index] = string[index].next
     end
-    string
-    #p string
+    index += 1
+  end
+  #p string
 end
 
 encrypt("abc")
+encrypt("zed")
 
-
-# - Get a string
-# - write a method with a loop inside of it
-# - Loop through each letter of the string
-# - Match each letter to the same letter in the alphabet
-# - get the index number for that letter in the alphabet, and and subtract one
-# - Match the new index number to the corresponding letter
-# - At the end, print the final string
+# Write a method with a loop inside of it
+# Loop through each letter of the string
+# Match each letter to the same letter in the alphabet string
+# Get the index number for that letter in the alphabet, and subtract one
+# Match the new index number to the corresponding letter
+# Print the final string
 
 def decrypt(string)
   index = 0
-  alphabet = "abcdefghijklmnopqrstuvwxyz"
-    while index < string.length
-      string[index] = alphabet[alphabet.index(string[index])-1]
-      index += 1
+  alphabet = ('a'..'z').to_a
+  while index < string.length
+    if string[index] == " "
+      string[index] = " "
+    else
+      string[index] = alphabet[alphabet.index(string[index].downcase)-1]
     end
-    string
-    # p string
+    index += 1
+  end
+  #p string
 end
 
 decrypt("bcd")
+decrypt("afe")
 
-decrypt(encrypt("The duck flies at midnight"))
+#p decrypt(encrypt("The duck flies at midnight"))
 
+#This nested call did not work at first because capital letters and spaces were not accounted for.  Now I have added conditional statements that specifies what it should do with spaces, and a rule to downcase each letter so that uppercase letters don't break it.  This does not re-caitalize the first letter, but I understand how this method works better than the previous method I had committed, so I am choosing to use this instead.
+
+#Ask user whether they would like to encrypt or decrypt a password and store their answer in a variable (called method)
+#Ask user to enter a password and store it in a variable (called password)
+#Create if/else that runs whichever method the user prefers, and print their encrypted or decrypted password.
 
 puts "Would you like to decrypt or encrypt a password?"
-  method = gets.chomp
+method = gets.chomp
 
 puts "Enter a password."
-  string = gets.chomp
+password = gets.chomp
 
-if method==decrypt
-  decrypt(string)
-elsif encrypt(string)
-  else puts "Try again"
+if method == "decrypt" || method == "d"
+  decrypt(password)
+elsif method == "encrypt" || method == "e"
+  encrypt(password)
+else
+  puts "Try again"
 end
 
-puts string
-
+p password
