fd8cfc673121132e7aef8796e473950ae01fc9d5
whatthemandy <amcdorman@gmail.com>
2016-03-29 17:21:12 -0500

    add methods

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,14 @@
+def build_array(x, y, z)
+  build_array = [x, y, z]
+  p build_array
+end
+
+def add_to_array(a, b)
+  add_to_array = (a << b).flatten
+  p add_to_array
+end
+
+
 array = []
 
 p array
@@ -14,7 +25,7 @@ array.delete_at(2)
 
 p array
 
-array.insert(2, "dos")
+array.insert(2, "tres")
 
 p array
 
@@ -27,10 +38,17 @@ if array.include?("four")
 else
 end
 
-another_array = [2, 4, 6, 8]
+another_array = [2, 3, 4, 5]
 
 third_array = array + another_array
 
 p third_array
+
+
+build_array(1, "two", nil)
+​
+add_to_array([], "a")
+add_to_array(["a", "b", "c", 1, 2], 3)
+add_to_array(["yas"], ["queen"])
 
 
