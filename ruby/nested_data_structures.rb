dd0ad45eb00e67be798bf4be1094796483faca99
whatthemandy <amcdorman@gmail.com>
2016-03-30 21:18:22 -0500

    first attempt at family hash

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,32 @@
+mcdorman = {
+  keith: {
+    relation: "big brother",
+    family: {
+      wife: "Rebecca",
+      children: "Dakota"
+    },
+      pets: ["Cooper", "Ziah"]
+  },
+
+  michael: {
+    relation: "middle brother",
+    family: {
+      wife: "Julianne",
+      children: "First one still in the oven"
+    },
+      pets: []
+  },
+
+  alex: {
+    relation: "little brother",
+    family: {
+      wife: [],
+      children: []
+    },
+      pets: ["Harley", "Zelda"]
+  }
+}
+
+
+
+p mcdorman
