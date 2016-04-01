e8c404b087e1d0767220963947826995a71b85a2
whatthemandy <amcdorman@gmail.com>
2016-04-01 15:59:35 -0500

    declare vowel and consonant arrays

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -5,9 +5,9 @@ name = gets.chomp
 #reverse first and last name
 name = name.split(" ").reverse.join(" ")
 
-#declare vowels and consonants strings
-vowels = "aeiou"
-consonants = "bcdfghjklmnpqrstvwxyz"
+#declare vowels and consonants arrays
+vowels = ["a", "e", "i", "o", "u"]
+consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
-p name.split
+
 
