cb88534adf0431a7195f2ce0b794bfe402f736dd
whatthemandy <amcdorman@gmail.com>
2016-03-31 21:38:05 -0500

    add hash and array methods

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -50,4 +50,38 @@ ages.map! { |age| age = age.next }
 p "After .map! change:"
 p ages
 
+#A method that iterates through the items, deleting any that meet a certain condition
+#(for example, deleting any numbers that are less than 5).
+
+#array:
+p ages.delete_if { |age| age < 20 }
+
+#hash:
+p favorite_color.delete_if { |name, color| color.length < 4 }
+
+#A method that filters a data structure for only items that do satisfy a certain condition
+#(for example, keeping any numbers that are less than 5).
+
+#array:
+p ages.select { |age| age < 20 }
+
+#hash:
+p favorite_color.reject { |name,color| color == "red"}
+
+#A different method that filters a data structure for only items satisfying a certain condition
+
+# array:
+p favorite_color.has_key?(:alex)
+p ages.values_at(1, 3, 4)
+
+#hash:
+p favorite_color.rassoc("gray")
+
+#A method that will remove items from a data structure until the condition in the block evaluates to false, then stops
+
+#array:
+p ages.keep_if { |age| age > 20 }
+
+#hash:
+p favorite_color.keep_if { |name, color| color == "green" }
 
