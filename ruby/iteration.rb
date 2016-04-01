824dd70612cdc97ece80d83bc1b9dfc49ba8aac6
whatthemandy <amcdorman@gmail.com>
2016-03-31 21:24:15 -0500

    iterate through hash and array with .each and .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,3 +1,5 @@
+ #Write your own method that takes a block
+
  def say_hello
    x = "Mandy"
    y = "Kyrene"
@@ -8,6 +10,8 @@
 
 say_hello { |x, y| puts "Hi #{x} and #{y}!" }
 
+#Declare an array and a hash, and populate each of them with some data
+
 favorite_color = {
   mandy: "gray",
   kyrene: "red",
@@ -17,3 +21,33 @@ favorite_color = {
 }
 
 ages = [27, 21, 25, 18, 19]
+
+#Demonstrate that you can iterate through each one using .each, and then using .map!:
+
+p "Before .each change:"
+p favorite_color
+
+p favorite_color.each do |name, color|
+  color = color.upcase
+end
+
+p "After .each change:"
+p favorite_color
+
+p "Before .each change:"
+p ages
+
+ages.each { |age| age = age.next }
+
+p "After .each change:"
+p ages
+
+p "Before .map! change:"
+p ages
+
+ages.map! { |age| age = age.next }
+
+p "After .map! change:"
+p ages
+
+
