832f717ed35352ffc3690e710b51321a90b356a4
whatthemandy <amcdorman@gmail.com>
2016-04-01 16:03:33 -0500

    create method for returning the next letter for each vowel/consonant

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,4 +10,19 @@ vowels = ["a", "e", "i", "o", "u"]
 consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
 
+#iterate through each letter
+#if space, keep space
+#if vowel, replace with next vowel
+#if consonant, replace with next consonant
 
+name.map! do |letter|
+  if letter == " "
+    letter = " "
+  elsif vowels.include?(letter)
+    letter = vowels[vowels.index(letter)+1]
+  elsif consonants.include?(letter)
+    letter = consonants[consonants.index(letter)+1]
+end
+end
+
+p name.join
