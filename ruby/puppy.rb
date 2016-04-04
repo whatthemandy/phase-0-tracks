683b094ef388cd888e1421bae032eef67af5a322
whatthemandy <amcdorman@gmail.com>
2016-04-04 17:23:04 -0500

    add class design in comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,18 @@
+# Puppies Class Design
+# CHARACTERISTICS ----------------
+# Type: varies
+# Color: varies
+# Friendly: yes
+# Leg count: 4
+
+# BEHAVIOR -----------------------
+# Run
+# Cuddle
+# Bark
+
+
+
+
+
+
+
