25907316cfc2857fceab4151b7cc84fe155de298
whatthemandy <amcdorman@gmail.com>
2016-04-05 17:23:29 -0500

    add instance methods to puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,3 +1,4 @@
+#puppy method
 class Puppy
 
   def fetch(toy)
@@ -5,5 +6,26 @@ class Puppy
     toy
   end
 
-end
+  def speak(x)
+    x = x.to_i
+    x.times {puts "Woof!"}
+  end
 
+  def rollover
+    puts "*roll over*"
+  end
+
+  def dog_years(human_year)
+    dog_year = human_year * 7
+    puts "Your dog is #{dog_year} years old."
+  end
+
+  def cuddle(time)
+    puts "Please cuddle with me for #{time} minutes."
+  end
+
+  def initialize
+    puts "Initializing new puppy instance..."
+  end
+
+end
