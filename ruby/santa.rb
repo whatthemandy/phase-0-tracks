b7581808f09ba48f4d782ce2fac20f3083c309cc
whatthemandy <amcdorman@gmail.com>
2016-04-05 20:17:32 -0500

    create santa class with speak, cookie, and initialize methods, test with driver code

---
+++ ruby/santa.rb
@@ -0,0 +1,20 @@
+class Santa
+
+  def speak
+    puts "Ho, ho, ho! Haaaappy holidays!"
+  end
+
+  def eat_milk_and_cookies(cookie_type)
+    puts "That was a good #{cookie_type}!"
+  end
+
+  def initialize
+    puts "Initializing Santa instance ..."
+  end
+
+end
+
+nick = Santa.new
+nick.speak
+nick.eat_milk_and_cookies
+
