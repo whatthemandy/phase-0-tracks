2759d11cf513591443b28350fb9af7f933cd69e7
whatthemandy <amcdorman@gmail.com>
2016-04-05 20:50:28 -0500

    update initialize method with attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,11 @@
 class Santa
+  def initialize(gender, ethnicity)
+    puts "Initializing Santa instance ..."
+    @gender = gender
+    @ethnicity = ethnicity
+    @reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+    @age = 0
+  end
 
   def speak
     puts "Ho, ho, ho! Haaaappy holidays!"
@@ -7,14 +14,10 @@ class Santa
   def eat_milk_and_cookies(cookie_type)
     puts "That was a good #{cookie_type}!"
   end
-
-  def initialize
-    puts "Initializing Santa instance ..."
-  end
-
 end
 
-nick = Santa.new
-nick.speak
-nick.eat_milk_and_cookies
+# nick = Santa.new
+# nick.speak
+# nick.eat_milk_and_cookies
+
 
