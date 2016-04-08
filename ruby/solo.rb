fc4af0f902e371504ca112b93fc6ff7489bd2ea0
whatthemandy <amcdorman@gmail.com>
2016-04-08 16:15:24 -0500

    write class instance methods

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -9,18 +9,31 @@
 # Inspire emotion (parameter: emotion)
 # Take time to read (parameter: hours)
 # Change your life
-# Teach you something new
 
 
 class Books
 
-  attr_reader :title :pages
+  attr_reader :title, :pages
   attr_accessor :quality
 
-  def initialize(title)
+  def initialize(title, pages)
     puts "Initializing new book: #{title}."
+    @title = title
+    @pages = pages
+  end
+
+  def inspire(emotion)
+    puts "'#{title}' made me feel #{emotion}."
+  end
+
+  def time_to_read(num_hours)
+    hours = hours.to_i
+    puts "'#{title}' takes an average of #{num_hours} hours to read."
+  end
+
+  def change_life
+    puts "'#{title}' changed my life!"
   end
 
 end
-
 
