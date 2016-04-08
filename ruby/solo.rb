ac6c2df07fd532f6596d6d6a5f8b6d32e3f136bd
whatthemandy <amcdorman@gmail.com>
2016-04-08 15:46:53 -0500

    add class design pseudocode

---
+++ ruby/solo.rb
@@ -0,0 +1,13 @@
+# Class Design Pseudocode - BOOKS
+
+# Characteristics/Attributes:
+# Amazing: yes (boolean)
+# Title: varies (string)
+# Number of pages: varies (integer)
+
+# Behavior/Methods:
+# Inspire emotion (parameter: emotion)
+# Take time to read (parameter: hours)
+# Change your life
+# Teach you something new
+
