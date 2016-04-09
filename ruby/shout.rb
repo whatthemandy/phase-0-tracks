07a27c7b6c47a04b6d15a50b1640fe636833f967
whatthemandy <amcdorman@gmail.com>
2016-04-08 21:15:40 -0500

    add yell angrily and happily methods with driver code to test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,13 @@
 module Shout
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yelling_happily(words)
+    words.upcase + "!" + " *fist-bump*"
+  end
 end
+
+puts yell_angrily("Oh, fiddlesticks")
+puts yelling_happily("yas queen")
 
