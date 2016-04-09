8e79b34d825379daab922bafa17a7c1b441dcae1
whatthemandy <amcdorman@gmail.com>
2016-04-08 21:54:34 -0500

    change module to misin

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,23 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#     words.upcase + "!" + " *fist-bump*"
+#   end
+# end
+
+# p Shout.yell_angrily("Oh, fiddlesticks")
+# p Shout.yelling_happily("yas queen")
+
+# change shout module to mixin
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+  def yell_angrily(words)
+    words.capitalize + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
+  def yelling_happily(words)
     words.upcase + "!" + " *fist-bump*"
   end
 end
-
-p Shout.yell_angrily("Oh, fiddlesticks")
-p Shout.yelling_happily("yas queen")
-
