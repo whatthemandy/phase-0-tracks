8557c479a97d7097d9d375e37030dd883d0fb9b3
whatthemandy <amcdorman@gmail.com>
2016-04-15 15:22:14 -0500

    add counting loops

--- js/explore.js
+++ js/explore.js
@@ -1,7 +1,16 @@
+// count up to ten and log each number:
+for (var i = 1; i < 11; i++) {
+  console.log(i);
+}
+
+// count down from ten and log each number:
+for (var i = 10; i > 0; i--) {
+  console.log(i);
+}
+
 // pseudocode a function that takes a string as a parameter and reverses the string:
 // iterate through string, starting with last object
 // print each object
-
 
 function reverse(string) {
   for (var i = string.length; i > 0; i--) {
