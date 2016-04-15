282c038f945ad1b04b175a96ce0055f9d2451df8
whatthemandy <amcdorman@gmail.com>
2016-04-14 20:08:03 -0500

    create horses object

--- js/data_structures.js
+++ js/data_structures.js
@@ -5,3 +5,12 @@ var names = ["Ed", "Bob", "Mike", "Pat"];
 // add to arrays
 colors.push("yellow");
 names.push("Sally");
+
+// use two arrays to make object using names as keys and colors as their values
+var horses = {};
+
+for (var index = 0; index < colors.length; index++) {
+    horses[names[index]] = colors[index];
+}
+console.log(horses);
+
