262db6ba6a284db9fd7acccf97f0435df98a7227
whatthemandy <amcdorman@gmail.com>
2016-04-14 22:20:50 -0500

    create car construction and test with driver code

--- js/data_structures.js
+++ js/data_structures.js
@@ -14,3 +14,22 @@ for (var index = 0; index < colors.length; index++) {
 }
 console.log(horses);
 
+// construction function for car
+function Car(type, model, year, isCool) {
+
+  console.log("Our new car:", this);
+
+  this.type = type;
+  this.model = model;
+  this.year = year;
+  this.isCool = isCool
+
+  this.sound = function() { console.log("vroooom"); };
+  this.price = function(price) {console.log ("This car costs " + price)};
+
+  console.log("CAR INITIALIZATION COMPLETE");
+}
+
+var new_car = new Car ("Honda", "Civic", 2000, true)
+new_car.price("$20,000")
+console.log(new_car)
