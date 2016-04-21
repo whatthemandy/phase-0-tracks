c85c2eec528223efad515688a4563bb8dd0a7164
whatthemandy <amcdorman@gmail.com>
2016-04-21 17:45:23 -0500

    remove author last name and genre

--- databases/challenge/books.rb
+++ databases/challenge/books.rb
@@ -7,9 +7,7 @@ create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS books(
     id INTEGER PRIMARY KEY,
     title VARCHAR(255),
-    author_fn VARCHAR(255),
-    author_ln VARCHAR(255),
-    genre VARCHAR(255),
+    author VARCHAR(255),
     read VARCHAR(255),
     rating INT,
     comment VARCHAR(255)
@@ -19,24 +17,24 @@ SQL
 db.execute(create_table_cmd)
 
 # add test book
-# db.execute("INSERT INTO books (title, author_fn, author_ln, genre, read, rating, comment) VALUES ('Enders Game', 'Orson Scott', 'Card', 'Science Fiction', 'yes', 5, 'Incredible!')")
+# db.execute("INSERT INTO books (title, author, read, rating, comment) VALUES ('Enders Game', 'Orson Scott Card', 'yes', 5, 'Incredible!')")
 
-def create_book(db, title, author_fn, author_ln, genre, read, rating, comment)
-  db.execute("INSERT INTO books (title, author_fn, author_ln, genre, read, rating, comment) VALUES (?, ?, ?, ?, ?, ?, ?)", [title, author_fn, author_ln, genre, read, rating, comment])
+def create_book(db, title, author, read, rating, comment)
+  db.execute("INSERT INTO books (title, author, read, rating, comment) VALUES (?, ?, ?, ?, ?)", [title, author, read, rating, comment])
 end
 
 def add(db)
   puts "Please enter title:"
   title = gets.chomp
 
-  puts "Please enter author's first/middle name:"
-  author_fn = gets.chomp
+  puts "Please enter author's name:"
+  author = gets.chomp
 
-  puts "Please enter author's last name:"
-  author_ln = gets.chomp
+  # puts "Please enter author's last name:"
+  # author_ln = gets.chomp
 
-  puts "Please enter book genre:"
-  genre = gets.chomp
+  # puts "Please enter book genre:"
+  # genre = gets.chomp
 
   puts "Have you read this book yet? (yes/no)"
   read = gets.chomp
@@ -53,7 +51,7 @@ def add(db)
   puts "Please enter any comments about book:"
   comment = gets.chomp
 
-  create_book(db, title, author_fn, author_ln, genre, read, rating, comment)
+  create_book(db, title, author, read, rating, comment)
 end
 
 def view(db)
