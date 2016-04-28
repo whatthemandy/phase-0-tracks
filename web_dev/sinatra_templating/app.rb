1b3ef6604521289cf07be518a2c372b362875089
whatthemandy <amcdorman@gmail.com>
2016-04-28 15:50:06 -0500

    add get/post to update student info

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -31,3 +31,16 @@ get '/san_fran' do
   @students = db.execute("SELECT * FROM students WHERE campus='SF'")
   erb :san_fran
 end
+
+# add form to get new edited student info
+get '/students/edit' do
+  erb :edit_student
+end
+
+# get info from edit form and update student info
+post '/student_edit' do
+  db.execute("UPDATE students SET name=?, campus=?, age=? WHERE name=?",
+             [params['new_name'], params['campus'], params['age'].to_i, params['current_name']])
+  redirect '/'
+end
+
