8859497179844a0339dcaf2ed51565e152d00067
whatthemandy <amcdorman@gmail.com>
2016-04-27 19:33:28 -0500

    add route that searches for all students in each campus

--- web_dev/sinatra_intro/app.rb
+++ web_dev/sinatra_intro/app.rb
@@ -77,3 +77,19 @@ get '/:num_one/plus/:num_two' do
   result.to_s
 end
 # localhost:9393/2/plus/5
+
+# Optional bonus: Make a route that allows the user to search the database in some way --
+# maybe for students who have a certain first name, or some other attribute. If you like,
+# you can simply modify the home page to take a query parameter, and filter the students
+# displayed if a query parameter is present.
+# -- I chose to display all the students from each campus by route parameter
+get '/student/:campus' do
+  students = db.execute("SELECT * FROM students WHERE campus=?", [params[:campus]])
+  result = ""
+  students.each do |student|
+    result << "Name: #{student['name']}<br>"
+    result << "Age: #{student['age']}<br><br>"
+  end
+  "<h1>#{params[:campus]} Students:</h1>#{result}"
+end
+# localhost:9393/student/SD
