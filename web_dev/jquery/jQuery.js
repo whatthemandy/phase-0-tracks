315bfeb19da84bfc26b37dd58f0fc3b0fd7783ce
whatthemandy <amcdorman@gmail.com>
2016-04-28 00:44:01 -0500

    add image and button to animate it

--- web_dev/jquery/jQuery.js
+++ web_dev/jquery/jQuery.js
@@ -5,4 +5,13 @@ $(document).ready(function(){
     $("#gibberish").toggle(2000);
   });
 
+  // click button to animate the image (push to the center/right and back)
+  $("#guess").click(function(){
+    var img = $("img");
+    img.animate({left:"500px"}, "slow");
+    img.animate({left:"1000px", opacity: '0.2'}, "slow");
+    img.animate({left:"500px", opacity: '1'}, "slow");
+    img.animate({left:"0px"}, "slow");
+  });
+
 });
